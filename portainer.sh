9c173eff06177606b77c771f906eee71bf7830dc
Andrew <abarabasz@gmail.com>
2023-04-06 23:37:05 +0200

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -4,7 +4,7 @@ docker volume create portainer
 docker pull portainer/portainer-ce:latest
 docker run -d -p 9443:9443 --name portainer \
     --restart=always \
-    -h portainer --domainname=barabasz.in \
+    -h portainer --domainname=YOURDOMAIN \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer:/data \
     portainer/portainer-ce
